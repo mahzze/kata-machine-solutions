eaea7a42939b5d53d18541e92b2198a8fbbbac10
mah <zanzinzzo@gmail.com>
2023-04-07 10:27:31 -0300

    crystall balls problem

---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,20 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+  let jmp = Math.floor(Math.sqrt(breaks.length))
+  let i = jmp
+
+  for (; i < breaks.length; i += jmp) {
+    if (breaks[i]) {
+      break
+    }
+  }
+
+  i -= jmp
+
+  for (let j = 0; j <= jmp && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) {
+      return i
+    }
+  }
+
+  return -1
+}
