ecd22b992effcb02cfc090328b890995e4f92b11
mah <zanzinzzo@gmail.com>
2023-04-09 01:38:08 -0300

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,44 @@
+type SNode<T> = {
+  value: T,
+  previous?: SNode<T>,
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: SNode<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const node = { value: item } as SNode<T>;
+
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    node.previous = this.head;
+    this.head = node;
+  }
+
+  pop(): T | undefined {
+
+    this.length = Math.max(0, this.length - 1);
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
+      return head?.value;
     }
 
-    push(item: T): void {
+    const head = this.head as SNode<T>;
+    this.head = head.previous;
+    return head.value;
+  }
 
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
