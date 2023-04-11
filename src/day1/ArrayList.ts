92e25a40f82effcb7589bc4a988890cd2f4f9834
mah <zanzinzzo@gmail.com>
2023-04-11 18:34:13 -0300

    start of arraylist (first structure developed without a guide)

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,55 @@
 export default class ArrayList<T> {
-    public length: number;
+  public length: number;
+  private capacity: number;
+  public array: Array<T>;
 
-    
+  constructor(capacity: number) {
+    this.length = 0;
+    this.capacity = capacity;
+    this.array = new Array(this.capacity);
+  }
 
-    constructor() {
+  prepend(item: T): void {
+
+  }
+
+  insertAt(item: T, idx: number): void {
+
+  }
+
+  append(item: T): void {
+    this.length++;
+    if (this.length === this.capacity) {
+      this.capacity = this.capacity * 2;
+      let arr = new Array(this.capacity)
+      for (let i = 0; i < this.length - 1; i++) {
+        arr[i] = this.array[i];
+      }
+      this.array = arr
     }
+    this.array[this.length] = item
+  }
 
-    prepend(item: T): void {
+  remove(item: T): T | undefined {
+    for (let i = 0; i < this.length; i++) {
+      if (item === this.array[i]) {
+        length--
+        return this.array[i]
+      }
+    }
+    return undefined
+  }
 
-}
-    insertAt(item: T, idx: number): void {
+  get(idx: number): T | undefined {
+    if (idx >= this.length) {
+      return undefined;
+    }
+    return this.array[idx]
+  }
 
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+  removeAt(idx: number): T | undefined {
+    if (idx >= this.length) {
+      return undefined
+    }
+  }
 }
